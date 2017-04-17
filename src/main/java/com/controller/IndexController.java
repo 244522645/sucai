868c98b71fef2c480199117ec1eab3edb98cc782
244522645@qq.com <z244522645>
2017-04-17 14:06:22 +0800

    1.1

---
+++ src/main/java/com/controller/IndexController.java
@@ -0,0 +1,10 @@
+package com.controller;
+
+/**
+ * Created by Administrator on 2017/4/17.
+ */
+public class IndexController {
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
+}
